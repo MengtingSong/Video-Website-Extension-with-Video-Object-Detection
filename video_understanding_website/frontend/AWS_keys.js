307e936d9e6d243eca85349f58e689b04cda3281
Mengting Song <martina.song@outlook.com>
2019-12-14 14:19:41 -0500

    Update AWS_keys.js

--- video_understanding_website/frontend/AWS_keys.js
+++ video_understanding_website/frontend/AWS_keys.js
@@ -1,6 +1,6 @@
 var AWS_credentials = {
-    accessKeyId :'AKIA4DYR3FCUHLFHUVPM',
-    secretAccessKey :'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'
+    accessKeyId :'',
+    secretAccessKey :''
 };
 // Remember do not upload the key info
 export {AWS_credentials};
